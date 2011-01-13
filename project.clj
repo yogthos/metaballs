877f61ed40c92c519e1863c21de7286fc3fd345b
Ghast <yogthos@gmail.com>
2011-01-13 17:54:13 -0500

    updated clojure dependency

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject metaballs "1.0"
   :description "Metaballs in Clojure."
-  :dependencies [[org.clojure/clojure "1.3.0-master-SNAPSHOT"]]
+  :dependencies [[org.clojure/clojure "1.3.0-alpha4"]]
   :main metaballs)
