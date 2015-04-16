d47d2736f8818998beb23e8b36d2c96cb41aa3d9
Yogthos <dmitri.sotnikov@gmail.com>
2015-04-16 01:05:56 -0400

    bumped up clj version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject metaballs "1.0"
   :description "Metaballs in Clojure."
-  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0-beta1"]]
   :main metaballs)
